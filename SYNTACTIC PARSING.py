8f0c357bd4d1404ab17fd1aa3983c10212b140fe
MahmoudSaeed <saeedmahmoud389@gmail.com>
2022-11-02 15:52:57 +0200

    Create SYNTACTIC PARSING.py

---
+++ SYNTACTIC PARSING.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 21 16:55:21 2022
+
+@author: Mahmoud Saeed
+"""
+
+
+with open("ai.en.txt") as f:
+    file = [line for line in f.readlines()]
+ 
+print(f)    
+
+
+
+
+
+
+
+
